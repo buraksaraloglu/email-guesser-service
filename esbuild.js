2513cacd6af14d8c120212177b0f94e616c507fe
Burak Saraloglu <buraksaraloglu1@gmail.com>
2022-06-15 02:48:10 +0300

    fix: esbuild native node modules error on build

--- esbuild.js
+++ esbuild.js
@@ -23,6 +23,42 @@ getFilesRecursively('src');
 
 const entryPoints = fileArray.filter((file) => file.endsWith('.ts') && !file.endsWith('.test.ts'));
 
+const nativeNodeModulesPlugin = {
+  name: 'native-node-modules',
+  setup(build) {
+    // If a ".node" file is imported within a module in the "file" namespace, resolve
+    // it to an absolute path and put it into the "node-file" virtual namespace.
+    build.onResolve({ filter: /\.node$/, namespace: 'file' }, (args) => ({
+      path: require.resolve(args.path, { paths: [args.resolveDir] }),
+      namespace: 'node-file'
+    }));
+
+    // Files in the "node-file" virtual namespace call "require()" on the
+    // path from esbuild of the ".node" file in the output directory.
+    build.onLoad({ filter: /.*/, namespace: 'node-file' }, (args) => ({
+      contents: `
+        import path from ${JSON.stringify(args.path)}
+        try { module.exports = require(path) }
+        catch {}
+      `
+    }));
+
+    // If a ".node" file is imported within a module in the "node-file" namespace, put
+    // it in the "file" namespace where esbuild's default loading behavior will handle
+    // it. It is already an absolute path since we resolved it to one above.
+    build.onResolve({ filter: /\.node$/, namespace: 'node-file' }, (args) => ({
+      path: args.path,
+      namespace: 'file'
+    }));
+
+    // Tell esbuild's default loading behavior to use the "file" loader for
+    // these ".node" files.
+    let opts = build.initialOptions;
+    opts.loader = opts.loader || {};
+    opts.loader['.node'] = 'file';
+  }
+};
+
 const pinoPlugin = (options) => ({
   name: 'pino',
   setup(currentBuild) {
@@ -119,9 +155,10 @@ const pinoPlugin = (options) => ({
 build({
   entryPoints,
   logLevel: 'info',
-  outdir: env === 'dev' ? 'dist' : 'build',
+  outdir: 'dist',
   bundle: env === 'dev' ? false : true,
   platform: 'node',
   format: 'cjs',
-  plugins: env === 'dev' ? [] : [pinoPlugin()]
+  plugins:
+    env === 'dev' ? [] : [nativeNodeModulesPlugin, pinoPlugin({ transport: ['pino-pretty'] })]
 });
